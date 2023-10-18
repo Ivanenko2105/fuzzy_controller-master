5dbc589ca5ff2085163cb2c93d8d94412efea068
Ivan Ivanenko <super-ivan-ivanenko@ukr.net>
2023-10-18 15:10:44 +0300

    refresh model

--- generate_rule_base.py
+++ generate_rule_base.py
@@ -1,37 +1,35 @@
 from model import input_lvs
 from itertools import product
 
-area = {"minimal": 0.25,
-		"modest": 0.5,
-		"comfortable": 0.75,
+area = {"minimal": 0,
+		"modest": 0.33,
+		"comfortable": 0.66,
 		"spacious": 1}
 
-floor = {"lower": 0.33,
-		"middle": 0.66,
-		"upper": 1}
+floor = {"lower": 0,
+		 "middle": 0.5,
+		 "upper": 1}
 
-age = {"new building": 1,
-		"young": 0.75,
-		"middle-aged": 0.5,
-		"old": 0.25}
+сondition = {"new": 1,
+		     "in good condition": 0.5,
+		     "needs repair": 0}
 
 distance = {"in the center": 1,
-			"near the center": 0.75,
-			"far from the center": 0.5,
-			"on the outskirts": 0.25}
+			"near the center": 0.66,
+			"far from the center": 0.33,
+			"on the outskirts": 0}
 
-coef = {"Area": 0.55,
+coef = {"Area": 0.4,
 		"Floor": 0.1,
-		"Аge of buildinge": 0.2,
+		"Condition": 0.35,
 		"Distance to the city center": 0.15}
 
-cost = {"extremely expensive": 0.95,
-		"luxurious": 0.9,
-		"elite": 0.8,
-		"high": 0.6,
-		"average": 0.5,
-		"affordable": 0.4,
-		"economical": 0}
+Class = {"luxurious": 0.85,
+		"expensive": 0.65,
+		"moderately expensive": 0.4,
+		"average price": 0.25,
+		"economical": 0.15,
+		"budget": 0}
 
 
 # Извлекаем имена терминов принадлежности для каждой переменной
@@ -40,18 +38,24 @@ term_names_lists = [list(var['terms'].keys()) for var in input_lvs]
 # Создаем список кортежей всех возможных комбинаций
 combinations = list(product(*term_names_lists))
 rule_base = []
+values = set()
 for comb in combinations:
 	area_coef = area[comb[0]] * coef["Area"]
 	floor_coef = floor[comb[1]] * coef["Floor"]
-	age_coef = age[comb[2]] * coef["Аge of buildinge"]
+	age_coef = сondition[comb[2]] * coef["Condition"]
 	distance_coef = distance[comb[3]] * coef["Distance to the city center"]
 	res = area_coef + floor_coef + age_coef + distance_coef
+	values.add(res)
 
-	for key, value in cost.items():
-		if res > value:
+	for key, value in Class.items():
+		if res >= value:
 			rule_base.append((comb, key))
 			break
 
+# for value in sorted(values):
+# 	print(value)
+
+# print(len(values))
 
 for rule in rule_base:
 	print(str(rule) + ",")
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import model
 import inference_mamdani
 import fuzzy_operators
 
-crisp = [90, 3, 2, 7]
+my_area = 50
+my_floor = 20
+my_rank = 7
+my_distance = 5
+
+crisp = [my_area, my_floor, my_rank, my_distance]
 inference_mamdani.preprocessing(model.input_lvs, model.output_lv)
 result = inference_mamdani.process(model.input_lvs, model.output_lv, model.rule_base, crisp)
 
